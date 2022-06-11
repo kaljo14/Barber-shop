4fc1ff1f6016c3fe41336e8002f6423554cf7029
kaljo14 <kaloyan.i.2000@icloud.com>
2022-06-11 23:05:12 +0300

    added admin view

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http;
+use App\Http\Middleware\Admin;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -63,5 +64,5 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    ];
+        'admin'=>Admin::class    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +20,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth','admin'])->name('admin.')->group(function(){
+    Route::get('/',[AdminController::class, 'index'])->name('index');
+});
 
 require __DIR__.'/auth.php';
