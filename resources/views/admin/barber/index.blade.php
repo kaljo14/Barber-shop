b9399dd4fc6e6b022c8f5e2f5e7cc02e074f16d8
kaljo14 <kaloyan.i.2000@icloud.com>
2022-06-21 18:28:58 +0300

    changing DB tables

--- resources/views/admin/barber/index.blade.php
+++ resources/views/admin/barber/index.blade.php
@@ -12,7 +12,7 @@
             </div>
 
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table class="w-full text-left w-full text-sm text-left text-gray-500">
+                <table class=" w-full text-sm text-left text-gray-500">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
                             <tr>
                                 <th scope="col" class="px-6 py-3">
